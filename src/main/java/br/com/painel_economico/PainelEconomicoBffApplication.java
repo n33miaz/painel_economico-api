10f24841e3b23d89074075bf5f4d9f6babaaedcc
n33miaz <ncormino@gmail.com>
2025-11-18 15:42:41 -0300

    First commit

---
+++ src/main/java/br/com/painel_economico/PainelEconomicoBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.painel_economico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelEconomicoBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelEconomicoBffApplication.class, args);
+	}
+
+}
