e5afcd157232c03289ed41ba992c4e4c9ee12648
n33miaz <ncormino@gmail.com>
2025-11-18 15:36:55 -0300

    First commit

---
+++ painel-economico-api/src/test/java/br/com/painel_economico_api/PainelEconomicoBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.painel_economico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelEconomicoBffApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
