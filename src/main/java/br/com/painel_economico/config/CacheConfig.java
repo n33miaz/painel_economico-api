83a2e260288d4555aca407518737672905b198dd
n33miaz <ncormino@gmail.com>
2025-12-09 19:38:26 -0300

    refactor: http connections

--- src/main/java/br/com/painel_economico/config/CacheConfig.java
+++ src/main/java/br/com/painel_economico/config/CacheConfig.java
@@ -1,13 +1,13 @@
 package br.com.painel_economico.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CacheConfig {
--- src/main/java/br/com/painel_economico/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/painel_economico/exception/GlobalExceptionHandler.java
@@ -1,19 +1,19 @@
 package br.com.painel_economico.exception;
 
-import br.com.painel_economico.dto.ErrorResponseDTO;
+import br.com.painel_economico.dto.ErrorResponse;
+import java.time.Instant;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.time.Instant;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(WebClientResponseException.class)
-    public ResponseEntity<ErrorResponseDTO> handleWebClientResponseException(WebClientResponseException ex) {
-        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+    public ResponseEntity<ErrorResponse> handleWebClientResponseException(WebClientResponseException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
                 ex.getStatusCode().value(),
                 "Erro ao se comunicar com o serviço externo: " + ex.getResponseBodyAsString(),
                 Instant.now());
